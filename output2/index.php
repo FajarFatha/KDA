44dbfab30268857cbdacbedf90653d3b8e4526f4
FajarFatha <fajarfathar@gmail.com>
2022-09-04 11:17:30 +0700

    menambahkan file sql dump

--- output2/index.php
+++ output2/index.php
@@ -4,17 +4,9 @@
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 <title>Login</title>
-<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto|Varela+Round">
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
-<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-<script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
-<link href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
-<script src="//netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
 <style>
-
 
 body {
 	color: #566787;
@@ -22,45 +14,6 @@ body {
 	font-family: 'Varela Round', sans-serif;
 	font-size: 13px;
 }
-
-.pagination {
-	float: right;
-	margin: 0 0 5px;
-}
-.pagination li a {
-	border: none;
-	font-size: 13px;
-	min-width: 30px;
-	min-height: 30px;
-	color: #999;
-	margin: 0 2px;
-	line-height: 30px;
-	border-radius: 2px !important;
-	text-align: center;
-	padding: 0 6px;
-}
-.pagination li a:hover {
-	color: #666;
-}	
-.pagination li.active a, .pagination li.active a.page-link {
-	background: #03A9F4;
-}
-.pagination li.active a:hover {        
-	background: #0397d6;
-}
-.pagination li.disabled i {
-	color: #ccc;
-}
-.pagination li i {
-	font-size: 16px;
-	padding-top: 6px
-}
-.hint-text {
-	float: left;
-	margin-top: 10px;
-	font-size: 13px;
-}    
-
 
 .container{
     width: 30%;
@@ -89,26 +42,6 @@ body {
 	  <button type="submit" class="btn btn-primary" value="LOGIN">login</button>
   </div>
 </form>
-</div>
-
-<!-- profile Modal -->
-<div id="profileModal" class="modal fade">
-	<div class="modal-dialog">
-		<div class="modal-content" >
-			<div class="modal-header">						
-				<h4 class="modal-title">Profileku</h4>
-				<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-			</div>
-			<div class="container mt-4 mb-4 p-3 d-flex justify-content-center" style="width: 300px;">
-				<div class="card p-4" style="width: 500px;">
-					<div class=" image d-flex flex-column justify-content-center align-items-center"> <button class="btn btn-secondary"> <img src="img/fajar.jpg" height="100" width="100" /></button> <span class="name mt-3">Fajar Fatha Romadhan</span> <span class="idd">@fajar_fatha</span>
-						<div class="d-flex flex-row justify-content-center align-items-center gap-2"> <span class="idd1">200411100047</span> <span></span> </div>
-						<div class="text mt-3"> <span>Universitas Trunojoyo Madura<br><br>Teknik Informatika</span> </div>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
 </div>
 
 </body>
